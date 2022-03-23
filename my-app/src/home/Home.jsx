cebe565aedff70e2be73f6d67dd833cc54da0c74
Pooja Kelkar <poojakelkar53@gmail.com>
2022-03-23 20:10:09 +0530

    feaure: creating navbar

---
+++ my-app/src/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navbar } from '../navbar/Navbar'
+import './home.scss'
+
+export const Home = () => {
+  return (
+    <div className='home'>
+        <Navbar></Navbar>
+    </div>
+  )
+}
---
+++ my-app/src/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './navbar.scss'
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">Navbar
+        <div className='container'>
+            <div className="left">
+                
+
+
+            </div>
+            <div className="right"></div>
+        </div>
+    </div>
+  )
+}
